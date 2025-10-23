bb03d0e3f317c2afb5e58b13116c24afb95e6969
Koushik25022005 <sripathikoushik244@gmail.com>
2025-10-23 08:33:40 +0400

    Asynchronous code

--- app.js
+++ app.js
@@ -1,10 +1,42 @@
-const http = require('http')
+const {readFile, writeFile} = require('fs')
+const util = require('util')
+const readFilePromise = util.promisify(readFile)
+const write
 
-const server = http.createServer((req, res) => {
-    console.log('request event')
-    res.end('Hello World')
-})
+// const getText = (path)=>{
+//     return new Promise((resolve, reject)=>{
+//         readFile(path, 'utf8', (err, data)=>{
+//             if (err){
+//                 reject(err)
+//                 return
+//             } else {
+//                 resolve(data)
+//             }
+//     })
+//     })
+// }
 
-server.listen(5000, () => {
-    console.log('Server listening on port : 5000.....')
-})
+// getText('./ontent/first.txt')
+// .then((result)=> console.log(result))
+// .catch((err)=>{console.log(err)})
+
+
+const start = async() =>{
+    try{
+        const result = await readFilePromise('./content/first.txt', 'utf8')
+        const second = await readFilePromise('./content/second.txt', 'utf8')
+        await writeFilePromise('./content/result-mind-grenade.txt',
+        `This is awesome: ${result} , ${second}`)
+        console.log(result, second)
+    } catch(error){
+        console.log(error)
+    }
+}
+
+start()
+.then(result=>{console.log(result)})
+.catch((err)=>{console.log(err)})
+
+
+//Promises help deal with asynchorous code in a better way
+//It can handle multiple asynchorous operations at the same time
